0b62a2e38262731fa00e6bb5bb8fa2f6cbfda7d1
Carlos Jorge Rodríguez Cuello <rodriguezcuelloc@gmail.com>
2022-08-24 13:27:07 -0400

    create temporal service for simulate de database

---
+++ src/app/services/in-memory-data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService {
+
+  constructor() { }
+  createDb() {
+    const users = [
+      { id: 1, firstName: 'Carlos J', lastMame: 'Rodriguez', email: 'carlito@gmail.com', password: '1234' },
+      { id: 2, firstName: 'Marlon', lastMame: 'Nomelose', email: 'marlito@gmail.com', password: '1234' }
+    ];
+    const posts = [
+      { id: 1, title: 'First Blog', author: 'AD', image: 'the-powerful-pickle-rick-photo', 
+      publishDate: '2022-08-01', excert: 'This is a summary of the content' }
+      ,{ id: 2, title: 'First Blog', author: 'AD', image: 'the-powerful-pickle-rick-photo', 
+      publishDate: '2022-08-01', excert: 'This is a summary of the content' }
+    ];
+  }
+}
