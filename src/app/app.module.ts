c8a24ba6145f91f65d775307f3113ed5893a1873
Carlos Jorge Rodríguez Cuello <rodriguezcuelloc@gmail.com>
2022-08-25 01:10:35 -0400

    create post service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './services/in-memory-data.service'
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
